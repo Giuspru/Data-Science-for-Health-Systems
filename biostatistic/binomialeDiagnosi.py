2bcf8346b322c50c7af51d3f146c00d5642172cc
giuspru <giuseppeprudente20@gmail.com>
2025-04-01 15:30:44 +0200

    :rocket: Some Checks

--- biostatistic/binomialeDiagnosi.py
+++ biostatistic/binomialeDiagnosi.py
@@ -25,3 +25,36 @@ if result < 0.05:
 else:
     print(f'Non ho una statistica significativa, quindi non rigettiamo ipotesi H0')
 
+def calculate_binomial(n, k, p):
+    n_fact = math.factorial(n)
+    k_fact = math.factorial(k)
+    n_k_fact = math.factorial(n - k)
+    p_k = p ** k
+    last_part = (1 - p) ** (n - k)
+
+    result = (n_fact / (k_fact * n_k_fact)) * p_k * last_part
+    return result
+
+a = calculate_binomial(100, 95, 0.99)
+print("\nquesto è quello ottenuto: " , result)
+
+
+def fact(n):
+    if n < 2:
+        return 1
+    else:
+        return n * fact(n - 1)
+    
+def calculate_binomial2(n, k, p):
+    n_fact = fact(n)
+
+    k_fact = fact(k)
+    n_k_fact = fact(n - k)
+    p_k = p ** k
+    last_part = (1 - p) ** (n - k)
+
+    result = (n_fact / (k_fact * n_k_fact)) * p_k * last_part
+    return result
+    
+b = calculate_binomial2(100, 95, 0.99)
+print("\nquesto è quello ottenuto: " , b)
