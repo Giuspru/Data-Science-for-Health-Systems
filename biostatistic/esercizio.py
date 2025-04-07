18de084f6bf0c0200f060c4bf39f4d807210d9b1
giuspru <giuseppeprudente20@gmail.com>
2025-04-07 16:22:40 +0200

    :construction:

---
+++ biostatistic/esercizio.py
@@ -0,0 +1,19 @@
+'''
+-Caricare il dataset.
+-EDA:
+    - Calcolo delle distribuzioni di frequenza.
+    - Calcolo delle frequenze cumulate relative (per darci una stima della CDF)
+    - Rappresentazioni grafiche: 
+        - pie chart sesso
+        - pie chart casi 
+    - Stima della PDF basandosi sugli istogrammi dell'età divisi in bin.
+    - Kernel Density smoothing: (1/(n*w))(sum(k(y-xi)/w)) dennsità di frequenza calcolata nel punto y (k è la funzione kernel e di default è gaussiano)
+
+
+- Una volta calcolata la FIMPRe possiamo trarre delle conclusioni: 
+    - Notiamo delle simmetrie?
+- FIMPRE e FIMPOST insieme
+
+- Funzione di Ripartizione empirica:
+
+'''
